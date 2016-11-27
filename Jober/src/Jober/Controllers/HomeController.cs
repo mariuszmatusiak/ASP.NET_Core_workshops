49e33e01b66412372ce1c81ce08b379d34941867
rafal.wasniewski@gmail.com <rafal.wasniewski@gmail.com>
2016-11-27 18:07:21 +0100

    STEP16 Caching

--- Jober/src/Jober/Controllers/HomeController.cs
+++ Jober/src/Jober/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Jober.Controllers
 {
@@ -6,33 +11,80 @@ namespace Jober.Controllers
     {
         private Data.IJobsRepository jobsRepository;
         private Data.ICitiesRepository citiesRepository;
-        public HomeController(Data.IJobsRepository jobsRepository, Data.ICitiesRepository citiesRepository)
+        private IMemoryCache memoryCache;
+        ILogger<HomeController> logger;
+
+        public HomeController(
+            Data.IJobsRepository jobsRepository,
+            Data.ICitiesRepository citiesRepository,
+            IMemoryCache memoryCache,
+            ILogger<HomeController> logger)
         {
             this.jobsRepository = jobsRepository;
             this.citiesRepository = citiesRepository;
+            this.memoryCache = memoryCache;
+            this.logger = logger;
         }
-       
+
         public IActionResult Index(string searchValue, int? CityId)
         {
-            ViewBag.Cities = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(citiesRepository.GetAll(), "CityId", "Name");
+            var cities = new List<Models.City>();
 
-            if (!string.IsNullOrWhiteSpace(searchValue)||CityId.HasValue)
+            if (memoryCache.TryGetValue<List<Models.City>>("Cities", out cities))
+            {
+                logger.LogInformation("Cities retrieved from cache.");
+            }
+            else
+            {
+                cities = citiesRepository.GetAll().ToList();
+                memoryCache.Set<List<Models.City>>(
+                    "Cities",
+                    cities,
+                    new MemoryCacheEntryOptions()
+                .SetAbsoluteExpiration(TimeSpan.FromMinutes(60)));
+                logger.LogInformation("Cities updated from DB.");
+            }
+
+            ViewBag.Cities = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(cities, "CityId", "Name");
+
+
+            if (!string.IsNullOrWhiteSpace(searchValue) || CityId.HasValue)
             {
                 return View(jobsRepository.Search(searchValue, CityId));
             }
-            return View(jobsRepository.GetAll());
+
+            var jobs = new List<Models.Job>();
+
+            if (memoryCache.TryGetValue<List<Models.Job>>("Jobs", out jobs))
+            {
+                logger.LogInformation("Jobs retrieved from cache.");
+            }
+            else
+            {
+                jobs = jobsRepository.GetAll().ToList();
+                memoryCache.Set<List<Models.Job>>(
+                    "Jobs",
+                    jobs,
+                    new MemoryCacheEntryOptions()
+                .SetAbsoluteExpiration(TimeSpan.FromMinutes(60)));
+                logger.LogInformation("Jobs updated from DB.");
+            }
+            return View(jobs);
         }
+
         public IActionResult Create()
         {
             ViewBag.Cities = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(citiesRepository.GetAll(), "CityId", "Name");
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(Models.Job item)
         {
             jobsRepository.Add(item);
             return RedirectToAction("Index");
         }
+
         public IActionResult Error()
         {
             return View();
